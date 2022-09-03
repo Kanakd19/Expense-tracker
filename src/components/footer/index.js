2fb36dd437bf8407925fd7968519b1e4a5eb7ded
Kanakd19 <107617907+Kanakd19@users.noreply.github.com>
2022-09-03 09:24:21 -0700

    Add files via upload

---
+++ src/components/footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return <footer>WELCOME</footer>;
+};
+
+export default Footer;
---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ExpenseList from "../../components/expense-list";
+
+import TopFold from "../../components/top-fold";
+import "./home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <TopFold />
+      <ExpenseList />
+    </div>
+  );
+};
+
+export default Home;
